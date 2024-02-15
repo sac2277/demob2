61f0fe76288d6ae602a69dd34b1c5a068acc0401
sac2277 <sachinnlohar@gmail.com>
2024-02-15 06:37:57 +0530

    first commit

---
+++ src/main/java/com/demo/DemoJunit2/Cal.java
@@ -0,0 +1,10 @@
+package com.demo.DemoJunit2;
+
+public class Cal {
+	public int doSum(int a,int b,int c) {
+		return a+b+c;
+	}
+	public int doMul(int a,int b) {
+		return a*b;
+	}
+}
---
+++ src/test/java/com/demo/DemoJunit2/CalTest.java
@@ -0,0 +1,44 @@
+package com.demo.DemoJunit2;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalTest {
+	Cal c=new Cal();
+	@BeforeAll
+	public static void setUpBeforeClass() {
+		System.out.println("before class....");
+	}
+	@BeforeEach
+	public void SetUp() {
+		System.out.println("Before Test.....");
+	}
+	
+	@Test
+	void testsum() {
+		int expRes=10;
+		int acRes=c.doSum(3, 3, 4);
+		Assert.assertEquals(acRes, expRes);
+	}
+	
+	@Test
+	void testMul() {
+		int expRes=17;
+		int acRes=c.doMul(3,5);
+		Assert.assertEquals(acRes, expRes);
+		
+	}
+	@AfterAll
+	public static void tearDownAfterClass() {
+		System.out.println("After class....");
+	}
+	@AfterEach
+	public void tearDown() {
+		System.out.println("after Test.....");
+	}
+
+}
